48f8473cb6574ac4742454a580b5c8fd9d359ace
rodrigoo-r <rodrigo@zelixlang.dev>
2026-09-17 12:45:07 -0500

    Add `storage_selection` enum and iterator implementations to `packed_vector` in `library.h`

--- library.h
+++ library.h
@@ -238,6 +238,15 @@ namespace zext
         }
     }
 
+    namespace config
+    {
+        enum class storage_selection
+        {
+            automatic,
+            manual
+        };
+    };
+
     namespace pmr
     {
         template <
@@ -246,7 +255,11 @@ namespace zext
 
             // The type of the elements we return when indexing/retrieving
             // NOTE: Not necessarily the elements in the vector, as those are bits
-            std::unsigned_integral T
+            std::unsigned_integral T,
+
+            // How the user prefers to select the inner storage that holds elements
+            config::storage_selection Storage = config::storage_selection::automatic,
+            std::unsigned_integral Word = std::uint32_t // Placeholder if automatic storage is selected
         >
         class packed_vector
         {
@@ -254,20 +267,24 @@ namespace zext
 
             using Inner_Container =
                 std::conditional_t<
-                    (bits_per_element < CHAR_BIT),
-                    // IMPORTANT: uint_fast32_t isn't always exactly 32 bits, but at least 32 bits
-                    uint_fast32_t,
+                    Storage == config::storage_selection::manual,
+                    Word,
                     std::conditional_t<
-                        (bits_per_element < (CHAR_BIT + (CHAR_BIT / 2))),
-                        // IMPORTANT: same as uint_fast32_t applies for uint_fast64_t
-                        uint_fast64_t,
-#                       ifdef __SIZEOF_INT128__
-                            // Compiler extension, not portable
-                            __uint128_t
-#                       else
-                            // Safe fallback
-                           uintmax_t
-#                       endif
+                        (bits_per_element < CHAR_BIT),
+                        // IMPORTANT: uint_fast32_t isn't always exactly 32 bits, but at least 32 bits
+                        std::uint_fast32_t,
+                        std::conditional_t<
+                            (bits_per_element < (CHAR_BIT + (CHAR_BIT / 2))),
+                            // IMPORTANT: same as uint_fast32_t applies for uint_fast64_t
+                            std::uint_fast64_t,
+    #                       ifdef __SIZEOF_INT128__
+                                // Compiler extension, not portable
+                                __uint128_t
+    #                       else
+                                // Safe fallback
+                               std::uintmax_t
+    #                       endif
+                        >
                     >
                 >
             ;
@@ -277,6 +294,163 @@ namespace zext
 
             std::pmr::vector<Slot> slots;
             size_t len = 0; // Number of active elements across all containers
+
+        public:
+            class iterator
+            {
+            public:
+                // STL Compatibility
+                using iterator_category = std::random_access_iterator_tag;
+                using value_type        = T;
+                using difference_type   = std::ptrdiff_t;
+                using pointer           = void;
+                using reference         = value_type;
+
+            private:
+                using Vector =
+                    packed_vector<Max_Value, T, Storage, Word>;
+
+                Vector *v_ptr;
+                size_t slot_idx = 0; // Index of the slot in the vector
+                size_t el_idx = 0; // Index of the element in the slot
+
+            public:
+                iterator(Vector *slot, size_t slot_idx, size_t el_idx) :
+                    v_ptr(slot), slot_idx(slot_idx), el_idx(el_idx)
+                {}
+
+                reference operator*() const
+                {
+                    auto &v_slots = v_ptr->slots;
+
+                    assert(slot_idx < v_slots.size());
+                    auto &slot = v_slots[slot_idx];
+                    assert(el_idx < slot.size());
+
+                    return slot.at(el_idx);
+                }
+
+                void operator->() const = delete;
+
+                // Prefix increment
+                auto &operator++()
+                {
+                    auto &v_slots = v_ptr->slots;
+                    assert(slot_idx < v_slots.size());
+
+                    auto &slot = v_slots[slot_idx];
+                    if (slot_idx == slot.size() - 1)
+                    {
+                        assert(slot_idx + 1 < v_slots.size());
+                        ++slot_idx;
+                        el_idx = 0;
+                    }
+                    else
+                    {
+                        ++el_idx;
+                        assert(el_idx < slot.size());
+                    }
+
+                    return *this;
+                }
+
+                // Postfix increment
+                auto operator++(int)
+                {
+                    auto tmp = *this;
+                    ++(*this);
+
+                    return tmp;
+                }
+
+                auto operator==(const iterator& other) const
+                {
+                    return v_ptr == other.v_ptr &&
+                        el_idx == other.el_idx &&
+                        slot_idx == other.slot_idx;
+                }
+            };
+
+            class const_iterator
+            {
+            public:
+                // STL Compatibility
+                using iterator_category = std::random_access_iterator_tag;
+                using value_type        = const T;
+                using difference_type   = std::ptrdiff_t;
+                using pointer           = void;
+                using reference         = value_type;
+
+            private:
+                using Vector =
+                    packed_vector<Max_Value, T, Storage, Word>;
+
+                using Const_Vector = const Vector;
+                using Const_Vector_Ptr = const Vector *const;
+
+                Const_Vector_Ptr v_ptr;
+                size_t slot_idx = 0; // Index of the slot in the vector
+                size_t el_idx = 0; // Index of the element in the slot
+
+            public:
+                const_iterator(Const_Vector_Ptr slot, size_t slot_idx, size_t el_idx) :
+                    v_ptr(slot), slot_idx(slot_idx), el_idx(el_idx)
+                {}
+
+                reference operator*() const
+                {
+                    auto &v_slots = v_ptr->slots;
+
+                    assert(slot_idx < v_slots.size());
+                    auto &slot = v_slots[slot_idx];
+                    assert(el_idx < slot.size());
+
+                    return slot.at(el_idx);
+                }
+
+                void operator->() const = delete;
+
+                // Prefix increment
+                auto &operator++()
+                {
+                    auto &v_slots = v_ptr->slots;
+                    assert(slot_idx < v_slots.size());
+
+                    auto &slot = v_slots[slot_idx];
+                    if (slot_idx == slot.size() - 1)
+                    {
+                        assert(slot_idx + 1 < v_slots.size());
+                        ++slot_idx;
+                        el_idx = 0;
+                    }
+                    else
+                    {
+                        ++el_idx;
+                        assert(el_idx < slot.size());
+                    }
+
+                    return *this;
+                }
+
+                // Postfix increment
+                auto operator++(int)
+                {
+                    auto tmp = *this;
+                    ++(*this);
+
+                    return tmp;
+                }
+
+                auto operator==(const const_iterator& other) const
+                {
+                    return v_ptr == other.v_ptr &&
+                        el_idx == other.el_idx &&
+                        slot_idx == other.slot_idx;
+                }
+            };
+
+        private:
+            friend class iterator;
 
         public:
             packed_vector(auto *allocator) :
@@ -305,6 +479,20 @@ namespace zext
             }
 
             void push_back(T value) { emplace_back(value); }
+
+            auto begin()    const noexcept { return __const_iterator(this, 0); }
+            auto end()      const noexcept { return __const_iterator(this, len); }
+
+            auto begin()    noexcept { return __iterator(this, 0); }
+            auto end()      noexcept { return __iterator(this, len); }
+            auto rbegin()   noexcept { return __iterator(this, len - 1); }
+            auto rend()     noexcept { return __iterator(this, -1); }
+
+            auto rbegin()   const noexcept { return __const_iterator(this, len - 1); }
+            auto rend()     const noexcept { return __const_iterator(this, -1); }
+
+            auto cbegin()   const noexcept { return __const_iterator(this, 0); }
+            auto cend()     const noexcept { return __const_iterator(this, len); }
         };
     }
 }
