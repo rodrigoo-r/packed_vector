66ec0f889a39d4591a2fd0ed243a9b95530a6f6e
rodrigoo-r <rodrigo@zelixlang.dev>
2026-09-17 11:08:02 -0500

    Initialize project with basic CMake setup and a minimal library implementation

---
+++ library.h
@@ -0,0 +1,20 @@
+//#==|--------------------------------------------------|==#
+//   *****              zext::packed_vector             *****
+//
+// A variation of the STL std::vector, exclusively for
+// unsigned integer or enum values.
+//
+// Version 0.0.1
+// https://github.com/rodrigoo-r/packed_vector
+//
+// Licensed under the Apache License v2.0
+// <https://www.apache.org/licenses/LICENSE-2.0>.
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2026 Rodrigo R. <rodrigo@zelixlang.dev>
+//
+// This file is an extension of the Zelix Programming
+// Language Backend, called Z; hence, the zext identifier.
+//#==|--------------------------------------------------|==#
+
+#pragma once
+
